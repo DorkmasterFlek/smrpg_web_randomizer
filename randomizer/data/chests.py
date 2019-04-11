802ed4aafc78c7e254cd056225ea527fc49dab77
DorkmasterFlek <dorkmasterflek@gmail.com>
2019-04-11 12:50:52 -0400

    Remove recovery mushroom chest in Mushroom Way for now because it refills itself

--- randomizer/data/chests.py
+++ randomizer/data/chests.py
@@ -1578,7 +1578,8 @@ def get_default_chests(world):
         MushroomWay1(world),
         MushroomWay2(world),
         MushroomWay3(world),
-        MushroomWay4(world),
+        # TODO: This chest refills when you re-enter the area and dups the item.  Uncomment when this is fixed.
+        # MushroomWay4(world),
         MushroomKingdomVault1(world),
         MushroomKingdomVault2(world),
         MushroomKingdomVault3(world),
