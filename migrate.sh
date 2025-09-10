e79e57809d698c1e7b465e4b1e22a07bc3d6a060
DorkmasterFlek <dorkmasterflek@gmail.com>
2025-09-10 00:01:36 -0400

    Working on containerizing

---
+++ migrate.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Apply migrations and collect static files for production build.
+docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
+docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
